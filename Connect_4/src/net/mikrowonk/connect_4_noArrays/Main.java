b4b19ecd245dd70bc5f88e29896152d07a9f3bdc
Microwonk <nicolas.theo.frey@gmail.com>
2023-01-10 13:20:25 +0100

    Initial commit

---
+++ Connect_4/src/net/mikrowonk/connect_4_noArrays/Main.java
@@ -0,0 +1,8 @@
+package net.mikrowonk.connect_4_noArrays;
+
+public class Main {
+    public static Game game = new Game();
+    public static void main(String[] args) {
+        game.startGame();
+    }
+}
